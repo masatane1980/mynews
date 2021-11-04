35052f1a48c6e9fc9b70c38068708a7ffccfe69c
masatane1980 <rgbdesigns_ytb@icloud.com>
2021-11-04 09:55:21 +0000

    2nd

---
+++ resources/views/admin/profile/create.blade.php
@@ -0,0 +1,55 @@
+@extends('layouts.admin')
+
+
+{{-- admin.blade.phpの@yield('title')に'ニュースの新規作成'を埋め込む --}}
+@section('title', 'プロフィールの新規作成')
+
+{{-- admin.blade.phpの@yield('content')に以下のタグを埋め込む --}}
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 mx-auto">
+                <h2>プロフィール新規作成</h2>
+                <form action="{{ action('Admin\ProfileController@create') }}" method="post" enctype="multipart/form-data">
+                    @if (count($errors) > 0)
+                        <ul>
+                            @foreach($errors->all() as $e)
+                                <li>{{ $e }}</li>
+                            @endforeach
+                        </ul>
+                    @endif
+                    <div class="form-group row">
+                        <label class="col-md-2">氏名</label>
+                        <div class="col-md-10">
+                            <input type="text" class="form-control" name="name" value="{{ old('name') }}">
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <div class="col-md-2">
+                            <form method="post" action="">性別
+                        </div>
+                            <div class="col-md-10">
+                                <label><input type="radio" name="gender" value="{{ old('gender', '男性') }}">男性</label>
+                                <label><input type="radio" name="gender" value="{{ old('gender', '女性') }}">女性</label>
+                            </div>
+                        </form>
+                    </div>
+                    <div class="form-group row">
+                        <label class="col-md-2">趣味</label>
+                        <div class="col-md-10">
+                            <input type="text" class="form-control" name="hobby" value="{{ old('hobby') }}">
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label class="col-md-2">自己紹介</label>
+                        <div class="col-md-10">
+                            <textarea class="form-control" name="introduction" rows="20">{{ old('introduction') }}</textarea>
+                        </div>
+                    </div>
+                    {{ csrf_field() }}
+                    <input type="submit" class="btn btn-primary" value="送信">
+                </form>
+            </div>
+        </div>
+    </div>
+@endsection
---
+++ resources/views/admin/profile/edit.blade.php
@@ -0,0 +1,60 @@
+@extends('layouts.admin')
+@section('title', 'プロフィールの編集')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 mx-auto">
+                <h2>プロフィール編集</h2>
+                <form action="{{ action('Admin\NewsController@update') }}" method="post" enctype="multipart/form-data">
+                    @if (count($errors) > 0)
+                        <ul>
+                            @foreach($errors->all() as $e)
+                                <li>{{ $e }}</li>
+                            @endforeach
+                        </ul>
+                    @endif
+                    <div class="form-group row">
+                        <label class="col-md-2" for="title">氏名</label>
+                        <div class="col-md-10">
+                            <input type="text" class="form-control" name="name" value="{{ $profile_form->name }}">
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <div class="col-md-2">
+                            <form method="post" action="">性別
+                        </div>
+                            <div class="col-md-10">
+                                <label><input type="radio" name="gender" value="{{ $profile_gender->gender }}">男性</label>
+                                <label><input type="radio" name="gender" value="{{ $profile_gender->gender }}">女性</label>
+                            </div>
+                        </form>
+                    </div>
+                    <div class="form-group row">
+                        <label class="col-md-2" for="body">趣味</label>
+                        <div class="col-md-10">
+                            <textarea class="form-control" name="hobby">{{ $profile_form->hobby }}</textarea>
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <div class="form-group row">
+                        <label class="col-md-2" for="body">自己紹介欄</label>
+                        <div class="col-md-10">
+                            <textarea class="form-control" name="introduction" rows="20">{{ $profile_form->introduction }}</textarea>
+                        </div>
+                    </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <div class="col-md-10">
+                            <input type="hidden" name="id" value="{{ $news_form->id }}">
+                            {{ csrf_field() }}
+                            <input type="submit" class="btn btn-primary" value="更新">
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+@endsection
